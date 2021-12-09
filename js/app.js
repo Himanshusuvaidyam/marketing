120e2b8e703d42df08323d23003d1d7f45c13457
himanshusuvaidyam <himanshu.suvaidyam@gmail.com>
2021-12-09 15:21:49 +0530

    add all file

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay:true,
+       slidesToShow:3,
+       slideToScroll:1,
+       prevArrow:".prev-btn",
+       nextArrow:".next-btn",
+       responsive:[
+           {
+               breakpoint:992,
+               settings:{
+                slidesToShow:1,
+               }
+           }
+       ]
+
+    });
+});
